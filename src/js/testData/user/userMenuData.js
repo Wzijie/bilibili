6d5afb62c7fe8cfcc45d88d7d50e683b958974c4
wzijie <981841273@qq.com>
2017-09-13 10:05:11 +0800

    v2 branch

--- src/js/testData/user/userMenuData.js
+++
@@ -1,8 +0,0 @@
-
-const userMenuData = [
-	{ title: '我的收藏', iconClass: 'fav' },
-	{ title: '我的投稿', iconClass: 'upload' },
-	{ title: '历史记录', iconClass: 'history' }
-]
-
-export default userMenuData;
